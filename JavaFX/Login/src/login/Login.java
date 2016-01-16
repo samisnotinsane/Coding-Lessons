ef5df4a1c586779d543c84e8e7d1ef8c2b6ebec8
sameenislam <sameenislam@192.168.1.11>
2016-01-16 19:41:19 +0000

    Added help text to form

--- JavaFX/Login/src/login/Login.java
+++ JavaFX/Login/src/login/Login.java
@@ -9,10 +9,12 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -56,6 +58,9 @@ public class Login extends Application {
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn, 1, 4);
+        
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
         
         Scene scene = new Scene(grid, 300, 275);
         primaryStage.setScene(scene);
