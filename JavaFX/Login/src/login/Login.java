ef35f177d8d8b7095d18d6353c9d12f8ac0d50a1
sameenislam <sameenislam@192.168.1.11>
2016-01-16 19:19:06 +0000

    Creating a simple form based on FX

---
+++ JavaFX/Login/src/login/Login.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sameenislam
+ */
+public class Login extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Welcome");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+        
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+        
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+        
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+        
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        //grid.setGridLinesVisible(true);
+        
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
