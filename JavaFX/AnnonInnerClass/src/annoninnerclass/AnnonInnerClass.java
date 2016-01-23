b4f2b52ea67debe2bc28fd27ee3608a9a6aeab4f
sameenislam <sameenislam@192.168.1.11>
2016-01-23 12:55:51 +0000

    Added Annon Inner Classes and Lambda Demo

---
+++ JavaFX/AnnonInnerClass/src/annoninnerclass/AnnonInnerClass.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package annoninnerclass;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sameenislam
+ */
+public class AnnonInnerClass extends Application {
+    
+    Button button;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Annonomous Inner Classes - Demo");
+        button = new Button();
+        button.setText("Click Me!"); // button will not display without putting it in a layout
+        
+        // event handler for the button
+        button.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Information");
+            alert.setHeaderText(null); // will cause a header banner to be displayed if not set to 'null'
+            alert.setContentText("Well Done!");
+            alert.showAndWait(); // won't be visible if this is left out
+        }
+        });
+        
+        StackPane layout = new StackPane(); // this will be the root node
+        layout.getChildren().addAll(button); // adds button to layout, still doesn't cause it to be displayed
+        Scene scene = new Scene(layout, 300.00, 250.00); // (rootNode, width, height)
+        primaryStage.setScene(scene); // sets the scene for this stage
+        primaryStage.show(); // display the stage
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
