eb2436d3960fbc3f3d803d2770e52b457b2a97da
sameenislam <sameenislam@192.168.1.11>
2016-01-23 17:08:01 +0000

    Added button event handler and resized SimplePad main screen.

--- JavaFX/SimplePad/src/simplepad/SimplePad.java
+++ JavaFX/SimplePad/src/simplepad/SimplePad.java
@@ -7,6 +7,10 @@ package simplepad;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -23,12 +27,43 @@ public class SimplePad extends Application {
         window = primaryStage;
         window.setTitle("New Note - SimplePad");
         
-        //GridPane with 10px padding around edge
+        //GridPane with padding around edge
         GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setPadding(new Insets(5, 10, 5, 10));
         grid.setVgap(8);
         grid.setHgap(10);
         
+        //Title Label - constrains use (child, column, row)
+        Label titleLabel = new Label("Title:");
+        GridPane.setConstraints(titleLabel, 0, 0);
+        
+        //Title Input
+        TextField titleInput = new TextField();
+        titleInput.setPromptText("Title");
+        titleInput.setMaxWidth(400); // having no effects
+        GridPane.setConstraints(titleInput, 1, 0);
+        
+        //Body Label
+        Label bodyLabel = new Label("Body:");
+        GridPane.setConstraints(bodyLabel, 0, 1);
+        
+        //Body Input
+        TextField bodyInput = new TextField();
+        bodyInput.setPromptText("Note");
+        bodyInput.setMaxHeight(400); // having no effects
+        GridPane.setConstraints(bodyInput, 1, 1);
+        
+        // Save
+        Button saveButton = new Button("Save");
+        GridPane.setConstraints(saveButton, 5, 30);
+        saveButton.setOnAction(e -> System.out.println("This feature is not ready yet."));
+        
+        //Add everything to grid
+        grid.getChildren().addAll(titleLabel, titleInput, bodyLabel, bodyInput, saveButton);
+
+        Scene scene = new Scene(grid, 350, 350);
+        window.setScene(scene);
+        window.show();
     }
 
     /**
