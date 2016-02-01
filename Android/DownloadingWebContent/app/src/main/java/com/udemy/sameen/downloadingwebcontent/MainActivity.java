4cf703051775bea6e7e1b49343742754d99e2411
sameenislam <sameenislam@192.168.1.11>
2016-02-01 02:50:38 +0000

    Created DownloadTask

--- Android/DownloadingWebContent/app/src/main/java/com/udemy/sameen/downloadingwebcontent/MainActivity.java
+++ Android/DownloadingWebContent/app/src/main/java/com/udemy/sameen/downloadingwebcontent/MainActivity.java
@@ -6,16 +6,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     public class DownloadTask extends AsyncTask<String, Void, String>{
         @Override
         protected String doInBackground(String... params) {
-            return null;
+            Log.i("URL", params[0]);
+            return "Done";
         }
     }
 
@@ -23,17 +27,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+        try {
+            result = task.execute("https://ecowebhosting.co.uk/").get();
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        Log.i("Result", result);
+
     }
 
     @Override
