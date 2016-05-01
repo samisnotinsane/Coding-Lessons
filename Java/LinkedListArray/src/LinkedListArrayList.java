42a483d11412d79e25578de64e72da9c87007336
sameenislam <sameenislam@192.168.1.11>
2016-05-01 12:14:57 +0100

    Implemented get() and set()

--- Java/LinkedListArray/src/LinkedListArrayList.java
+++ Java/LinkedListArray/src/LinkedListArrayList.java
@@ -4,15 +4,18 @@ import javafx.scene.control.Cell;
  * Created by sameenislam on 01/05/2016.
  * An ArrayList Abstract Data Type (ADT) based on a LinkedList Data Structure (DS).
  */
-public class LinkedListArrayList <E>{
+public class LinkedListArrayList <E>
+{
 
     private Cell<E> myList;
 
-    public LinkedListArrayList() {
+    public LinkedListArrayList()
+    {
         this.myList = null;
     }
 
-    public void add(E item) {
+    public void add(E item)
+    {
         if(myList==null)
             myList = new Cell<E>(item,null);
         else {
@@ -29,10 +32,28 @@ public class LinkedListArrayList <E>{
         return count;
     }
 
+    public E get(int pos)
+    {
+        Cell<E> ptr=myList;
+        for(int count=0; count<pos&&ptr!=null; ptr=ptr.next, count++) {}
+        if(ptr==null)
+            throw new IndexOutOfBoundsException();
+        return ptr.first;
+    }
+
+    public void set(int pos, E item)
+    {
+        Cell<E> ptr=myList;
+        for(int count=0; count<pos&&ptr!=null; ptr=ptr.next, count++) {}
+        if(ptr==null)
+            throw new IndexOutOfBoundsException();
+        ptr.first=item;
+    }
 
 
     // Custom LinkedList implementation: 'Cell' (nested recursive class)
-    private static class Cell<T> {
+    private static class Cell<T>
+    {
         T first;
         Cell<T> next;
 
