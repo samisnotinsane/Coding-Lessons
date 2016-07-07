03c3aedf0e1bd13a1e09a005a0eeee956bc086b1
sameenislam <sameenislam@192.168.1.11>
2016-07-07 21:36:10 +0100

    Added move next logic.

---
+++ Android/Contacts/app/src/main/java/com/sameen/contacts/MainActivity.java
@@ -0,0 +1,123 @@
+package com.sameen.contacts;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+/* Author: Sameen
+ * Data source: androidhive.info
+ * Date: July 7 2016
+ */
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Contacts";
+    private static final String WEB_URL = "http://api.androidhive.info/contacts/";
+    private TextView txtId, txtName, txtEmail, txtSex;
+
+    // holds downloaded data
+    private String[] iDs, names, emails, sexes;
+    // pointer for arrays
+    private int iDsPointer, namesPointer, emailsPointer, sexesPointer;
+
+    // current fields
+    private int id;
+    private String name;
+    private String email;
+    private String sex;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialise
+        txtId = (TextView) findViewById(R.id.txtId);
+        txtName = (TextView) findViewById(R.id.txtName);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        txtSex = (TextView) findViewById(R.id.txtSex);
+
+        iDsPointer = 0;
+        namesPointer = 0;
+        emailsPointer = 0;
+        sexesPointer= 0;
+    }
+
+    public void onClickNextBtn(View view) {
+        Log.d(TAG, "Next button tapped.");
+
+        // move pointers forward
+        iDsPointer++;
+        namesPointer++;
+        emailsPointer++;
+        sexesPointer++;
+
+
+
+        // check if all items were downloaded; retrieve from cache if it weren't
+        if(iDs[5] == null) {
+
+            // data not yet downloaded
+            ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if(networkInfo != null && networkInfo.isConnected()) {
+                // fetching data as there is a conn. available
+                Toast.makeText(getBaseContext(),"Fetching...", Toast.LENGTH_SHORT).show();
+//                new DataDownloadThread().execute(WEB_URL);
+            } else {
+                // no network connection available
+                Toast.makeText(getBaseContext(),"No network connection!", Toast.LENGTH_LONG).show();
+                Log.i("Error", "No network connection!");
+            }
+        } else {
+
+            // data already downloaded
+            if (iDsPointer < iDs.length - 1) {
+                id = Integer.parseInt(iDs[iDsPointer]);
+                name = names[namesPointer];
+                email = emails[emailsPointer];
+                sex = sexes[sexesPointer];
+            } else {
+                resetAllPointers();
+            }
+
+        }
+
+
+    }
+
+    public void onClickPrevBtn(View view) {
+        Log.d(TAG, "Previous button tapped.");
+    }
+
+    public void resetAllPointers() {
+        iDsPointer = 0;
+        namesPointer = 0;
+        emailsPointer = 0;
+        sexesPointer = 0;
+    }
+
+//    private class DataDownloadThread extends AsyncTask<String, Void, String> {
+//        @Override
+//        protected String doInBackground(String... urls) {
+//            // params comes from the execute() call: params[0] is the url.
+//            try {
+////                return downloadUrl(urls[0]); // contains downloaded content
+//            } catch (IOException e) {
+//                return "Unable to retrieve web page. URL may be invalid.";
+//            }
+//        }
+//    }
+
+}
+
+
--- JavaFX/ChangingText/src/Main.java
+++ JavaFX/ChangingText/src/Main.java
@@ -1,5 +1,50 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
 /**
  * Created by sameenislam on 30/06/2016.
  */
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        Label lbl = new Label();
+        lbl.setText("Time elapsed: ");
+
+
+        Pane root = new Pane();
+        btn.setLayoutX(75);
+        btn.setLayoutY(120);
+        root.getChildren().addAll(btn,lbl);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
