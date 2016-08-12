928de0724ae96d7226389974c57dd2f2a9c20a72
sameenislam <sameenislam@192.168.1.11>
2016-08-12 19:40:09 +0100

    Adding Toolbar to actionbar and fixing positioning.

--- Android/AppBar/app/src/main/java/com/sameen/appbar/MainActivity.java
+++ Android/AppBar/app/src/main/java/com/sameen/appbar/MainActivity.java
@@ -2,6 +2,8 @@ package com.sameen.appbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
