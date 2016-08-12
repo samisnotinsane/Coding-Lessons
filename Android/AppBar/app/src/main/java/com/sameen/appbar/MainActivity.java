6534c8e497023a1f95469337ae2272f913083322
sameenislam <sameenislam@192.168.1.11>
2016-08-12 20:21:42 +0100

    Fixing toolbar theme and adding buttons, including overflow.

--- Android/AppBar/app/src/main/java/com/sameen/appbar/MainActivity.java
+++ Android/AppBar/app/src/main/java/com/sameen/appbar/MainActivity.java
@@ -1,18 +1,29 @@
 package com.sameen.appbar;
 
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(myToolbar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
 }
